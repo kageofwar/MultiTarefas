997ffb039dc95e02b0145515f5d2904faeda3b70
Vinicius Casotti de Campos Oliveira <viniciusccoliveira@PREFEITURA.SP.GOV.BR>
2023-12-14 18:00:45 -0300

    neymar

--- src/views/calculadorDeDividas/Criar.vue
+++ src/views/calculadorDeDividas/Criar.vue
@@ -5,71 +5,110 @@
         </RouterLink>
 
         <div class="flex justify-center">
-            <h1 class="text-[black] font-[Inter] mb-[30px]"> Nome do Lançamento </h1>
+            <h1 class="font-[Inter] text-[#EBD7C6] text-[25px] mb-[50px]">Nome do Lançamento</h1>
         </div>
 
-        <section class="flex justify-center">
-            <div class="grid grid-cols-2 w-[95%] h-[35vh]">
-                <div class="flex bg-[white] justify-center">
-                    <div class="overflow-x-auto w-[70%]">
-                        <table class="table">
+        <section class="flex justify-center h-[vh]">
+            <div class="grid grid-cols-[60%_1fr] w-[95%] h-[35vh]">
+                <div class="flex justify-center h-[35vh]">
+                    <div class="flex justify-center overflow-y-auto w-[95%]">
+                        <table class="table w-[80%]">
                             <thead>
-                                <tr>
+                                <tr class="bg-[black] text-[#EBD7C6]">
                                     <th>Nome</th>
+                                    <th>Data</th>
                                     <th>Valor</th>
                                     <th>Ação</th>
                                 </tr>
                             </thead>
-                            <tbody>
+                            <tbody class="text-[black] text-[20px]">
                                 <tr>
-                                    <td>Cy Ganderton</td>
-                                    <td>Quality Control Specialist</td>
-                                    <td>Blue</td>
+                                    <td>Mercado</td>
+                                    <td>09/04/2002</td>
+                                    <td>R$ 498,33</td>
+                                    <td><button><img src="../../assets/icons/shield-xmark-svgrepo-com (1).svg"
+                                                alt="botao de excluir"></button></td>
                                 </tr>
                                 <tr>
-                                    <td>Hart Hagerty</td>
-                                    <td>Desktop Support Technician</td>
-                                    <td>Purple</td>
+                                    <td>Mercado</td>
+                                    <td>09/04/2002</td>
+                                    <td>R$ 498,33</td>
+                                    <td><button><img src="../../assets/icons/shield-xmark-svgrepo-com (1).svg"
+                                                alt="botao de excluir"></button></td>
                                 </tr>
                                 <tr>
-                                    <td>Brice Swyre</td>
-                                    <td>Tax Accountant</td>
-                                    <td>Red</td>
+                                    <td>Mercado</td>
+                                    <td>09/04/2002</td>
+                                    <td>R$ 498,33</td>
+                                    <td><button><img src="../../assets/icons/shield-xmark-svgrepo-com (1).svg"
+                                                alt="botao de excluir"></button></td>
                                 </tr>
                                 <tr>
-                                    <td>Brice Swyre</td>
-                                    <td>Tax Accountant</td>
-                                    <td>Red</td>
+                                    <td>Mercado</td>
+                                    <td>09/04/2002</td>
+                                    <td>R$ 498,33</td>
+                                    <td><button><img src="../../assets/icons/shield-xmark-svgrepo-com (1).svg"
+                                                alt="botao de excluir"></button></td>
                                 </tr>
                                 <tr>
-                                    <td>Cy Ganderton</td>
-                                    <td>Quality Control Specialist</td>
-                                    <td>Blue</td>
+                                    <td>Mercado</td>
+                                    <td>09/04/2002</td>
+                                    <td>R$ 498,33</td>
+                                    <td><button><img src="../../assets/icons/shield-xmark-svgrepo-com (1).svg"
+                                                alt="botao de excluir"></button></td>
                                 </tr>
                                 <tr>
-                                    <td>Hart Hagerty</td>
-                                    <td>Desktop Support Technician</td>
-                                    <td>Purple</td>
+                                    <td>Mercado</td>
+                                    <td>09/04/2002</td>
+                                    <td>R$ 498,33</td>
+                                    <td><button><img src="../../assets/icons/shield-xmark-svgrepo-com (1).svg"
+                                                alt="botao de excluir"></button></td>
                                 </tr>
                                 <tr>
-                                    <td>Brice Swyre</td>
-                                    <td>Tax Accountant</td>
-                                    <td>Red</td>
-                                </tr>
-                                <tr>
-                                    <td>Brice Swyre</td>
-                                    <td>Tax Accountant</td>
-                                    <td>Red</td>
+                                    <td>Mercado</td>
+                                    <td>09/04/2002</td>
+                                    <td>R$ 498,33</td>
+                                    <td><button><img src="../../assets/icons/shield-xmark-svgrepo-com (1).svg"
+                                                alt="botao de excluir"></button></td>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
-    
-                <div class="bg-[white] flex flex-col justify-center place-items-center">
+
+                <div class="flex flex-col justify-center place-items-center text-[black]">
                     <h2 class="text-[150%]">Total Inserido</h2>
                     <h2 class="text-[400%]">R$ 1495,00</h2>
                     <p>Total Lançamentos: 12</p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="flex justify-center">
+                <h2 class="text-[20px] text-[#EBD7C6] font-[Inter] m-[25px]">Pessoas Cadastradas</h2>
+            </div>
+            <div class="grid grid-cols-[25vh_25vh_25vh_25vh_25vh] gap-24 place-content-center place-items-center">
+                <div class="card bg-[black] h-[200px] w-[200px]">
+                    <div class="flex justify-center">
+                        <h2 class="text-[#EBD7C6] font-[inter]">Nome da pessoa</h2>
+                    </div>
+                    <table class="table">
+                        <thead>
+                            <tr class="bg-[black] place-content-center">
+                                <th class="text-[#EBD7C6]">Titulo</th>
+                                <th class="text-[#EBD7C6]">Valor</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-[#EBD7C6]">
+                            <tr class="hover text-[black] font-[inter]">
+                                <td>Riot Points</td>
+                                <td>R$ 79,90</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="flex h-[25vh] justify-center border-[10px] border-[#EBD7C6] rounded h-[70px] w-[70px]">
+                    <button><img src="../../assets/icons/user-plus-alt-1-svgrepo-com.svg" alt=""></button>
                 </div>
             </div>
         </section>
@@ -79,3 +118,5 @@
 <script>
 
 </script>
+
+<style></style>
--- src/views/calculadorDeDividas/Visualizar.vue
+++ src/views/calculadorDeDividas/Visualizar.vue
@@ -3,10 +3,10 @@
     <RouterLink to="/" class="btn bg-[#EBD7C6] text-[black]"><img src="../../assets/icons/back-square-svgrepo-com.svg"
         alt="icone voltar" class="h-[4vh]">Voltar para o inicio</RouterLink>
     <div class="flex flex-col items-center justify-center">
-      <div class="flex items-center justify-center mb-[40px]">
+      <div class="flex items-center justify-center mb-[20px]">
         <h1 class="font-[Inter] text-[#EBD7C6] text-[50px]">Historico</h1>
       </div>
-      <p class="text-[#EBD7C6] font-[Inter]">Escreva em somente em <span style="color: #f00;">um</span> ou nos <span style="color: #f00;">dois</span> campos abaixo e clique em <span style="color: #f00;">Pesquisar</span>.</p>
+      <p class="text-[black] font-[Inter]">Escreva em somente em <span style="color: #f00;">um</span> ou nos <span style="color: #f00;">dois</span> campos abaixo e clique em <span style="color: #f00;">Pesquisar</span>.</p>
       <div class="flex space-x-8 m-[20px] text-[#EBD7C6]">
         <label for="nome" class="flex flex-col justify-center items-center">Nome
           <input type="text" placeholder="Pesquisar por nome" class="input input-bordered w-full max-w-xs text-[white] placeholder:text-[#EBD7C6]" />
@@ -15,9 +15,9 @@
           <input type="text" placeholder="Pesquisar por data" class="input input-bordered w-full max-w-xs text-[white] placeholder:text-[#EBD7C6]" />
         </label>
       </div>
-      <button type="submit" class="btn m-[20px] text-[#EBD7C6]">Pesquisar<img src="../../assets/icons/magnifying-glass-svgrepo-com.svg" alt="lupa de pequisar" class="h-[30px]"></button>
+      <button type="submit" class="btn mb-[10px] text-[#EBD7C6]">Pesquisar<img src="../../assets/icons/magnifying-glass-svgrepo-com.svg" alt="lupa de pequisar" class="h-[30px]"></button>
       <span v-if="loading == true" class="loading loading-spinner loading-lg mt-[100px] bg-[black]"></span >
-      <div v-else="loading == false" class="overflow-x-auto h-[50vh] w-[70%]">
+      <div v-else="loading == false" class="overflow-x-auto h-[40vh] w-[70%]">
         <table class="table text-[black] place-content-center">
           <thead>
             <tr class="bg-[black] place-content-center">
@@ -29,27 +29,6 @@
             </tr>
           </thead>
           <tbody class="bg-[#EBD7C6]">
-            <tr v-for="lancamento in lancamentos" :key="lancamento.id" class="hover">
-              <td>{{ lancamento.id }}</td>
-              <td>{{ lancamento.nome_lancamento }}</td>
-              <td>{{ lancamento.valor }}</td>
-              <td>{{ lancamento.data }}</td>
-              <td><button>VISUALIZAR</button></td>
-            </tr>
-            <tr v-for="lancamento in lancamentos" :key="lancamento.id" class="hover">
-              <td>{{ lancamento.id }}</td>
-              <td>{{ lancamento.nome_lancamento }}</td>
-              <td>{{ lancamento.valor }}</td>
-              <td>{{ lancamento.data }}</td>
-              <td><button>VISUALIZAR</button></td>
-            </tr>
-            <tr v-for="lancamento in lancamentos" :key="lancamento.id" class="hover">
-              <td>{{ lancamento.id }}</td>
-              <td>{{ lancamento.nome_lancamento }}</td>
-              <td>{{ lancamento.valor }}</td>
-              <td>{{ lancamento.data }}</td>
-              <td><button>VISUALIZAR</button></td>
-            </tr>
             <tr v-for="lancamento in lancamentos" :key="lancamento.id" class="hover">
               <td>{{ lancamento.id }}</td>
               <td>{{ lancamento.nome_lancamento }}</td>
